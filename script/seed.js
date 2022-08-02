c809f6cc4f4452cbdc0ca54cc0e52713137307d7
Nelson Cheng-Lin <nelsonchenglin@gmail.com>
2022-08-02 15:17:11 -0400

    pokemon schema set up

--- script/seed.js
+++ script/seed.js
@@ -1,47 +1,62 @@
-'use strict'
+"use strict";
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User, Mons },
+} = require("../server/db");
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+    User.create({ username: "cody", password: "123" }),
+    User.create({ username: "murphy", password: "123" }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  // Create Mons
+
+  const mons = await Promise.all([
+    Mons.create({ name: "Bulbasaur", price: 3.99 }),
+    Mons.create({ name: "Ivysaur", price: 4.99 }),
+    Mons.create({ name: "Venasaur", price: 6.99 }),
+  ]);
+
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded successfully`);
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+    mons: {
+      Bulbasaur: mons[0],
+      Ivysaur: mons[1],
+      Venasaur: mons[2],
+    },
+  };
 }
-
 /*
  We've separated the `seed` function from the `runSeed` function.
  This way we can isolate the error handling and exit trapping.
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -50,9 +65,10 @@ async function runSeed() {
   `Async` functions always return a promise, so we can use `catch` to handle
   any errors that might occur inside of `seed`.
 */
+
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,17 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
+const Mons = require("./models/Mons");
 
-const User = require('./models/User')
+const User = require("./models/User");
 
 //associations could go here!
 
 module.exports = {
   db,
+  Mons,
   models: {
     User,
+    Mons,
   },
-}
+};
