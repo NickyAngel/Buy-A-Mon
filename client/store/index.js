ee069e1f9db5013c74668f9fed44f96c9830d15c
Apple <apple@apples-mbp.mynetworksettings.com>
2022-08-02 22:39:05 -0400

    initial redux files

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,15 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import itemsReducer from './items'
+import cartReducer from './cart'
 
-const reducer = combineReducers({ auth })
+const reducer = combineReducers({ 
+  auth,
+  items: itemsReducer,
+  singleItem: singleItemReducer,
+  cart: cartReducer,
+ })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
