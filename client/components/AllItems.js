d6cef9fc7cefe223d476d45c8905277eace3a33e
Nelson Cheng-Lin <nelsonchenglin@gmail.com>
2022-08-04 15:09:51 -0400

    display all items working route/allitems

--- client/components/AllItems.js
+++ client/components/AllItems.js
@@ -1,63 +1,44 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchAllItems, deleteItem, createItem  } from "../store";
+import { deleteItem, createItem } from "../store";
+import { fetchAllItems } from "../store/items";
 
 export class AllItems extends React.Component {
   constructor() {
     super();
     this.state = {
-      quantity: 1
+      quantity: 1,
     };
-    this.addToCart = this.addToCart.bind(this);
+    // this.addToCart = this.addToCart.bind(this);
   }
   componentDidMount() {
     this.props.getItems();
   }
   render() {
+    let { items } = this.props;
     return (
       <div>
-      <h1>Pokemon Cards</h1>
-      
-{userLoggedIn.role === 'admin' ? (
-         <div>
-               <Button
-                  className="create"
-                  onClick={() => createItem (item.id)}
-                >
-                 Add Item
-                </Button>
+        <h1>Pokemon Cards</h1>
 
-                  <Button
-                  className="delete"
-                  onClick={() => deleteItem(item.id)}
-                >
-                  Delete Product
-                </Button>
+        <div id="allItems">
+          {items.map((item) => {
+            return (
+              <div className="allItems" key={item.id}>
+                <Link to={`/items/${item.id}`}>
+                  {item.name}
+                  <img height="400vh" width="400vh" src={item.imageUrl} />
+                </Link>
+                <div>
+                  <h3>Price: ${item.price / 100}</h3>
+                </div>
+              </div>
+            );
+          })}
 
-                <Button
-                  className="delete"
-                  onClick={() => editItem(item.id)}
-                >
-                  Edit Item
-                </Button>
-       </div> ):
-}
-    
-     <div id = 'allItems'>
-        {this.props.items.map((items) => (
-          <div className="allItems" key={items.id}>
-            <Link to={`/items/${items.id}`}>
-              {items.name}
-              <img height="400vh" width="400vh" src={items.imageUrl} />
-            </Link>
-            {items.descriptions}
-          </div>
-        ))}
-
-        <button>
-          <Link to="/BuyAMon/signup">SIGN UP</Link>
-        </button>
+          <button>
+            <Link to="/BuyAMon/signup">SIGN UP</Link>
+          </button>
         </div>
       </div>
     );
@@ -72,9 +53,27 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     getItems: () => dispatch(fetchAllItems()),
-    deleteItem: (itemId) => dispatch(deleteItem(itemId)),
-    addItem: (itemId) => dispatch (createItem (itemId))
+    // deleteItem: (itemId) => dispatch(deleteItem(itemId)),
+    // addItem: (itemId) => dispatch(createItem(itemId)),
   };
 };
 
 export default connect(mapState, mapDispatch)(AllItems);
+
+// {userLoggedIn.role === "admin" ? (
+//   <div>
+//     <Button className="create" onClick={() => createItem(item.id)}>
+//       Add Item
+//     </Button>
+
+//     <Button className="delete" onClick={() => deleteItem(item.id)}>
+//       Delete Product
+//     </Button>
+
+//     <Button className="delete" onClick={() => editItem(item.id)}>
+//       Edit Item
+//     </Button>
+//   </div>
+// ) : (
+//   "placeholder"
+// )}
