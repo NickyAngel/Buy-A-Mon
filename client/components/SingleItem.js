a38f5e7f438b2765eb91860e9e967da2b0e24ba4
Nick Angelopoulos <neangelopoulos@gmail.com>
2022-08-08 14:35:07 -0600

    guest cart add and edit functionality working

--- client/components/SingleItem.js
+++ client/components/SingleItem.js
@@ -1,14 +1,71 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { fetchSingleItem } from "../store/singleItem";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchSingleItem } from '../store/singleItem';
+import { addItemToCart } from '../store/cart';
+import { fetchCart } from '../store/cart';
+import { me } from '../store/auth';
 
 class SingleItem extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  async handleClick(event) {
+    event.preventDefault();
+
+    const user = await this.props.getUser();
+    if (!user) {
+      let alreadyInCart = false;
+      if (!window.localStorage.getItem('cart')) {
+        window.localStorage.setItem('cart', JSON.stringify([]));
+      }
+      let guestCart = JSON.parse(window.localStorage.getItem('cart'));
+      for (let i = 0; i < guestCart.length; i++) {
+        if (guestCart[i].id === this.props.item.id && guestCart[i].qty) {
+          guestCart[i].qty++;
+          alreadyInCart = true;
+        }
+      }
+      if (!alreadyInCart) {
+        let temp = this.props.item;
+        temp.qty = 1;
+        guestCart.push(temp);
+      }
+      window.localStorage.setItem('cart', JSON.stringify(guestCart));
+      console.log(window.localStorage.cart);
+    } else {
+      const id = user.auth.id;
+      let addQty = parseInt(event.target);
+
+      if (!addQty) {
+        addQty = 1;
+      }
+
+      const newItemOrder = {
+        id: this.props.item.id,
+        name: this.props.item.name,
+        price: this.props.item.price,
+        imageUrl: this.props.item.imageUrl,
+        description: this.props.item.description,
+        qty: addQty,
+      };
+      this.props.addItem(newItemOrder, id);
+    }
+  }
   // componentDidMount() {
   //   this.props.singleItem(this.props.match.params.id);
   // }
-  componentDidMount() {
-    this.props.singleItem(this.props.match.params.id);
+  async componentDidMount() {
+    const user = await this.props.getUser();
+    if (user) {
+      const id = user.auth.id;
+      this.props.singleItem(this.props.match.params.id);
+      this.props.getCart(id);
+    } else {
+      this.props.singleItem(this.props.match.params.id);
+    }
   }
 
   render() {
@@ -23,20 +80,25 @@ class SingleItem extends React.Component {
         <h1>{name}</h1>
         <h4>${price / 100}</h4>
         <h4>{description}</h4>
-        <Link to="/pokemon/addToCart"> Add to Cart</Link>
+        <button onClick={this.handleClick} type="submit">
+          Add To Cart
+        </button>
       </div>
     );
   }
 }
 
-const mapState = (state) => {
+const mapState = state => {
   return {
     cart: state.cart,
     item: state.singleItem,
   };
 };
-const mapDispatch = (dispatch) => ({
-  singleItem: (id) => dispatch(fetchSingleItem(id)),
+const mapDispatch = dispatch => ({
+  singleItem: id => dispatch(fetchSingleItem(id)),
+  addItem: (item, userId) => dispatch(addItemToCart(item, userId)),
+  getCart: id => dispatch(fetchCart(id)),
+  getUser: () => dispatch(me()),
 });
 
 export default connect(mapState, mapDispatch)(SingleItem);
--- client/components/cart.js
+++ client/components/cart.js
@@ -9,22 +9,125 @@ export class Cart extends React.Component {
     super(props);
     this.state = {
       id: null,
+      guestCart: [],
     };
   }
   async componentDidMount() {
     const user = await this.props.getUser();
-    const userId = user.auth.id;
-    this.setState({ id: userId });
-    this.props.getCart(userId);
+    if (user) {
+      const userId = user.auth.id;
+      this.setState({ id: userId });
+      this.props.getCart(userId);
+    }
+    let guestCart = JSON.parse(window.localStorage.getItem('cart'));
+    // console.log(guestCart);
+    if (guestCart) {
+      this.setState({ guestCart: guestCart });
+    }
   }
 
   render() {
     let { cart } = this.props || {};
-    console.log(this.props);
-
-    return (
+    let guestJSX = (
+      <div>
+        <h1>Guest Cart</h1>
+        {this.state.guestCart.length === 0 ? (
+          <h2>Your Cart is empty</h2>
+        ) : (
+          <span />
+        )}
+        <div id="cartItems">
+          {this.state.guestCart.map(item => {
+            return (
+              <div className="cartItems" key={item.id}>
+                <Link to={`/items/${item.id}`}>
+                  {item.name}
+                  <img height="100vh" width="100vh" src={item.imageUrl} />
+                </Link>
+                <div>
+                  <h3>Total Price: ${(item.price * item.qty) / 100}</h3>
+                  <h3>Quantity: {item.qty}</h3>
+                </div>
+                <button
+                  onClick={evt => {
+                    let guestCart = JSON.parse(
+                      window.localStorage.getItem('cart')
+                    );
+                    for (let i = 0; i < guestCart.length; i++) {
+                      if (guestCart[i].id === item.id) {
+                        guestCart[i].qty++;
+                      }
+                    }
+                    window.localStorage.setItem(
+                      'cart',
+                      JSON.stringify(guestCart)
+                    );
+                    this.setState({ guestCart: guestCart });
+                  }}
+                >
+                  Add 1
+                </button>
+                {item.qty > 1 ? (
+                  <button
+                    onClick={evt => {
+                      let guestCart = JSON.parse(
+                        window.localStorage.getItem('cart')
+                      );
+                      for (let i = 0; i < guestCart.length; i++) {
+                        if (guestCart[i].id === item.id) {
+                          guestCart[i].qty--;
+                        }
+                      }
+                      window.localStorage.setItem(
+                        'cart',
+                        JSON.stringify(guestCart)
+                      );
+                      this.setState({ guestCart: guestCart });
+                    }}
+                  >
+                    Minus 1
+                  </button>
+                ) : (
+                  <span />
+                )}
+                <button
+                  onClick={evt => {
+                    let guestCart = JSON.parse(
+                      window.localStorage.getItem('cart')
+                    );
+                    for (let i = 0; i < guestCart.length; i++) {
+                      if (guestCart[i].id === item.id) {
+                        guestCart.splice(i, 1);
+                        break;
+                      }
+                    }
+                    window.localStorage.setItem(
+                      'cart',
+                      JSON.stringify(guestCart)
+                    );
+                    this.setState({ guestCart: guestCart });
+                  }}
+                >
+                  Remove Pokemon
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          onClick={() => {
+            this.setState({ guestCart: [] });
+            window.localStorage.removeItem('cart');
+          }}
+        >
+          <Link to="/checkout">CHECKOUT</Link>
+        </button>
+      </div>
+    );
+    let userJSX = (
       <div>
         <h1>Cart</h1>
+        {cart.length === 0 ? <h2>Your Cart is empty</h2> : <span />}
         <div id="cartItems">
           {cart.map(item => {
             //console.log(item);
@@ -38,7 +141,6 @@ export class Cart extends React.Component {
                   <h3>Total Price: ${item.totalPriceAtSaleTime / 100}</h3>
                   <h3>Quantity: {item.qty}</h3>
 
-                  {/* UPDATE CART FEATURE */}
                   <button
                     onClick={evt => {
                       this.props.update(
@@ -66,18 +168,21 @@ export class Cart extends React.Component {
                   >
                     Remove Pokemon
                   </button>
-
-                  {/* UPDATE CART FEATURE */}
                 </div>
               </div>
             );
           })}
         </div>
-        <button>
+        <button
+          onClick={() => {
+            this.props.closeCart(this.state.id);
+          }}
+        >
           <Link to="/checkout">CHECKOUT</Link>
         </button>
       </div>
     );
+    return this.state.id ? userJSX : guestJSX;
   }
 }
 const mapState = state => {
@@ -93,6 +198,7 @@ const mapDispatch = dispatch => {
     update: (thingToUpdate, userId) =>
       dispatch(updateCart(thingToUpdate, userId)),
     delete: (itemId, userId) => dispatch(clearItem(itemId, userId)),
+    //closeCart: id => dispatch(closeCart(id)),
   };
 };
 
