ad110ce3bd80f1852965d8a5a4a69bfe81f3c418
Nick Angelopoulos <neangelopoulos@gmail.com>
2022-08-09 15:26:36 -0600

    bug fixes for show all users

--- server/api/users.js
+++ server/api/users.js
@@ -1,13 +1,14 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
   models: { User, Order, OrderItem, Item },
-} = require("../db");
+} = require('../db');
 module.exports = router;
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const requireToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
+    console.log(token);
     const user = await User.findByToken(token);
     req.user = user;
     next();
@@ -16,21 +17,21 @@ const requireToken = async (req, res, next) => {
   }
 };
 const adminCheck = (req, res, next) => {
-  if (req.user.role === "admin") {
+  if (req.user.role === 'admin') {
     next();
   } else {
-    return res.status(403).send("YOU SHALL NOT PASS");
+    return res.status(403).send('YOU SHALL NOT PASS');
   }
 };
 //getting all users by ID? not sure where we would do this
 //GET api/users/
-router.get("/", requireToken, adminCheck, async (req, res, next) => {
+router.get('/', requireToken, adminCheck, async (req, res, next) => {
   try {
     const users = await User.findAll({
       //   // explicitly select only the id and email fields - even though
       //   // users' passwords are encrypted, it won't help if we just
       //   // send everything to anyone who asks!
-      attributes: ["id", "email", "firstName", "lastName", "role"],
+      attributes: ['id', 'email', 'firstName', 'lastName', 'role'],
     });
     res.json(users);
   } catch (err) {
@@ -40,7 +41,7 @@ router.get("/", requireToken, adminCheck, async (req, res, next) => {
 
 //Grabbing a users data/profile when logged in
 //GET api/users/:id
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.id);
     res.json(user);
@@ -51,7 +52,7 @@ router.get("/:id", async (req, res, next) => {
 
 //Create a new user row to the User table
 //POST api/users/
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
     //decide what the req body looks like
     const user = await User.create(req.body);
@@ -63,7 +64,7 @@ router.post("/", async (req, res, next) => {
 
 //Update the user once the form is updated
 //PUT api/users/:id
-router.put("/:id", async (req, res, next) => {
+router.put('/:id', async (req, res, next) => {
   try {
     const cart = await Order.findOne({
       where: { userId: req.params.id, open: true },
@@ -89,7 +90,7 @@ router.put("/:id", async (req, res, next) => {
 
 //Delete the user if the user wants the account to be deleted
 //DELETE api/users/:id
-router.delete("/:id", async (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.id);
     await user.destroy(req.params.id);
@@ -104,7 +105,7 @@ router.delete("/:id", async (req, res, next) => {
 //grab the cart per single user
 //GET api/users/:id/cart/
 router.get(
-  "/:id/cart/",
+  '/:id/cart/',
   /*requireToken,*/ async (req, res, next) => {
     try {
       // const tokenFromFrontEnd = req.headers.authorization;
@@ -118,7 +119,7 @@ router.get(
       });
       const itemDetails = [];
       await Promise.all(
-        items.map(async (item) => {
+        items.map(async item => {
           let eachMon = await Item.findByPk(item.itemId);
           eachMon.dataValues.priceAtSaleTime = item.price;
           eachMon.dataValues.qty = item.qty;
@@ -136,7 +137,7 @@ router.get(
 
 //Update the cart per item added to each cart
 //PUT api/users/:id/cart/
-router.put("/:id/cart/", async (req, res, next) => {
+router.put('/:id/cart/', async (req, res, next) => {
   try {
     const cart = await Order.findOne({
       where: { userId: req.params.id, open: true },
@@ -164,7 +165,7 @@ router.put("/:id/cart/", async (req, res, next) => {
     });
     const itemDetails = [];
     await Promise.all(
-      items.map(async (item) => {
+      items.map(async item => {
         let eachMon = await Item.findByPk(item.itemId);
         eachMon.dataValues.priceAtSaleTime = item.price;
         eachMon.dataValues.qty = item.qty;
@@ -181,7 +182,7 @@ router.put("/:id/cart/", async (req, res, next) => {
 
 // Are we trying to destroy carts per user? or maybe we can empty a cart at checkout
 //PUT api/users/:id/cart/ EMPTY CART AT CHECKOUT
-router.put("/:id/cart/", async (req, res, next) => {
+router.put('/:id/cart/', async (req, res, next) => {
   try {
     console.log(req.params);
     const cart = await Order.findOne({
@@ -195,7 +196,7 @@ router.put("/:id/cart/", async (req, res, next) => {
     });
     const itemDetails = [];
     await Promise.all(
-      items.map(async (item) => {
+      items.map(async item => {
         let eachMon = await Item.findByPk(item.itemId);
         eachMon.dataValues.priceAtSaleTime = item.price;
         eachMon.dataValues.qty = item.qty;
@@ -209,7 +210,7 @@ router.put("/:id/cart/", async (req, res, next) => {
   }
 });
 
-router.post("/:id/cart", async (req, res, next) => {
+router.post('/:id/cart', async (req, res, next) => {
   try {
     let cart = await Order.findOne({
       where: { userId: req.params.id, open: true },
@@ -249,7 +250,7 @@ router.post("/:id/cart", async (req, res, next) => {
   }
 });
 //DELETE ROUTE FOR CART ITEM
-router.delete("/:id/cart/:itemId", async (req, res, next) => {
+router.delete('/:id/cart/:itemId', async (req, res, next) => {
   try {
     console.log(req.params);
     const cart = await Order.findOne({
@@ -263,7 +264,7 @@ router.delete("/:id/cart/:itemId", async (req, res, next) => {
     });
     const itemDetails = [];
     await Promise.all(
-      items.map(async (item) => {
+      items.map(async item => {
         let eachMon = await Item.findByPk(item.itemId);
         eachMon.dataValues.priceAtSaleTime = item.price;
         eachMon.dataValues.qty = item.qty;
