604f3fc7bc1fc7c40205c68dfeb5c631a9fc4af2
Nick Angelopoulos <neangelopoulos@gmail.com>
2022-08-09 15:14:15 -0600

    bug fixes on Admin create pokemon functionality

--- client/store/items.js
+++ client/store/items.js
@@ -1,23 +1,23 @@
-import axios from "axios"
+import axios from 'axios';
 
 let initialState = [];
 
-let CREATE_ITEM = "CREATE_ITEM";
-let SET_ITEMS = "SET_ITEMS";
-let DELETE_ITEM = "DELETE_ITEM";
-let UPDATE_ITEM = "UPDATE_ITEM";
-let ADD_ITEM = "ADD_ITEM";
+let CREATE_ITEM = 'CREATE_ITEM';
+let SET_ITEMS = 'SET_ITEMS';
+let DELETE_ITEM = 'DELETE_ITEM';
+let UPDATE_ITEM = 'UPDATE_ITEM';
+let ADD_ITEM = 'ADD_ITEM';
 
 //ACTION CREATOR: SET ALL ITEMS
-export const setAllItems = (items) => {
-  console.log(items[0])
+export const setAllItems = items => {
+  console.log(items[0]);
   return {
     type: SET_ITEMS,
     items,
   };
 };
 //ACTION CREATOR: SET ALL ITEMS
-export const createitem = (items) => {
+export const createitem = items => {
   return {
     type: CREATE_ITEM,
     items,
@@ -26,9 +26,9 @@ export const createitem = (items) => {
 
 //THUNK: GRAB ALL ITEMS
 export const fetchAllItems = () => {
-  return async (dispatch) => {
+  return async dispatch => {
     try {
-      const { data } = await axios.get("/api/items/");
+      const { data } = await axios.get('/api/items/');
       dispatch(setAllItems(data));
     } catch (err) {
       console.log(err);
@@ -37,17 +37,17 @@ export const fetchAllItems = () => {
 };
 
 //ADD ITEM (ADMINISTRATOR)
-export const addItem = (item) => {
+export const addItem = item => {
   return {
     type: ADD_ITEM,
     item,
   };
 };
 //THUNK ADD ITEM (ADMINISTRATOR)
-export const createItem = (item) => {
-  return async (dispatch) => {
+export const createItem = item => {
+  return async dispatch => {
     try {
-      const { data } = await axios.post(`/api/items/:id/create/`, item);
+      const { data } = await axios.post(`/api/items/create/`, item);
       dispatch(addItem(data));
     } catch (err) {
       console.log(err);
@@ -56,8 +56,8 @@ export const createItem = (item) => {
 };
 
 //UPDATE ITEM (ADMINISTRATOR)
-export const reformItem = (item) => {
-  console.log("afterthunk");
+export const reformItem = item => {
+  console.log('afterthunk');
   return {
     type: UPDATE_ITEM,
     item,
@@ -65,8 +65,8 @@ export const reformItem = (item) => {
 };
 //THUNK: PUT REQUEST
 export const updateItem = (item, id) => {
-  console.log("before thunk");
-  return async (dispatch) => {
+  console.log('before thunk');
+  return async dispatch => {
     const { data } = await axios.put(`/api/items/${id}`, item);
     //const { data } = await axios.get("/api/items/");
     dispatch(reformItem(data));
@@ -74,15 +74,15 @@ export const updateItem = (item, id) => {
 };
 
 //ACTION CREATOR: REMOVE A ITEM
-export const removeItem = (id) => {
+export const removeItem = id => {
   return {
     type: DELETE_ITEM,
     id,
   };
 };
 //THUNK: DELETE REQUEST
-export const deleteItem = (id) => {
-  return async (dispatch) => {
+export const deleteItem = id => {
+  return async dispatch => {
     try {
       await axios.delete(`/api/items/${id}/`);
       dispatch(removeItem(id));
@@ -100,11 +100,11 @@ export default function itemsReducer(state = initialState, action) {
     case ADD_ITEM:
       return [...state, action.item];
     case UPDATE_ITEM:
-      return action.item
+      return action.item;
     case CREATE_ITEM:
       return [...state, action.campus];
     case DELETE_ITEM:
-      return state.filter((Item) => Item.id !== action.id);
+      return state.filter(Item => Item.id !== action.id);
     default:
       return state;
   }
--- server/api/items.js
+++ server/api/items.js
@@ -1,25 +1,24 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
   models: { Item },
-} = require("../db");
+} = require('../db');
 module.exports = router;
 
 //Grabbing all items from the DB
 //api/items/
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const items = await Item.findAll();
-   
+
     res.json(items);
   } catch (err) {
     next(err);
   }
 });
 
-
 //Grabbing a single item from the DB
 //api/items/:id
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const item = await Item.findByPk(req.params.id);
     res.json(item);
@@ -31,33 +30,37 @@ router.get("/:id", async (req, res, next) => {
 //Create the item as an admin
 //POST api/items/:id/
 
-// router.post("/:id/create/", async (req, res, next) => {
-//   try {
-//     //decide what the req body looks like
-//     const item = await Item.create(req.body);
-//     res.status(201).send(item);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-//Update the item as an admin
-//PUT api/items/:id/
-
-router.put("/:id/", async (req, res, next) => {
+router.post('/create/', async (req, res, next) => {
   try {
-    const item = await Item.findByPk(req.params.id);
-    const picture = req.body.imageUrl == '' ? item.imageUrl : req.body.imageUrl
-    const summary = req.body.description == '' ? item.description : req.body.description
-    
-    await item.update({...item, ...req.body, imageUrl: picture, description: summary})
-
-    res.send(item)
+    const item = await Item.create(req.body);
+    res.status(201).send(item);
   } catch (err) {
     next(err);
   }
 });
 
+//Update the item as an admin
+//PUT api/items/:id/
+
+router.put('/:id/', async (req, res, next) => {
+  try {
+    const item = await Item.findByPk(req.params.id);
+    const picture = req.body.imageUrl == '' ? item.imageUrl : req.body.imageUrl;
+    const summary =
+      req.body.description == '' ? item.description : req.body.description;
+
+    await item.update({
+      ...item,
+      ...req.body,
+      imageUrl: picture,
+      description: summary,
+    });
+
+    res.send(item);
+  } catch (err) {
+    next(err);
+  }
+});
 
 //Delete the item if admin is deleting
 //DELETE api/items/:id
